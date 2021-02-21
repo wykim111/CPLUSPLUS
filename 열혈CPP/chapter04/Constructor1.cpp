c9cdc2bd003621ee97ee962f5f44045862a0ef52
wykim111 <40811068+wykim111@users.noreply.github.com>
2021-02-21 22:05:32 +0900

    Create Constructor1.cpp

---
+++ 열혈CPP/chapter04/Constructor1.cpp
@@ -0,0 +1,48 @@
+/*
+	생성자의 오버로딩이 가능
+*/
+#include<iostream>
+
+using namespace std;
+
+class SimpleClass
+{
+private:
+	int num1;
+	int num2;
+public:
+	SimpleClass()
+	{
+		num1 = 0;
+		num2 = 0;
+	}
+	SimpleClass(int n)
+	{
+		num1 = n;
+		num2 = 0;
+	}
+	SimpleClass(int n1, int n2)
+	{
+		num1 = n1;
+		num2 = n2;
+	}
+
+	void ShowData() const
+	{
+		cout << num1 << ' ' << num2 << endl;
+	}
+};
+
+int main()
+{
+	SimpleClass sc1;
+	sc1.ShowData();
+
+	SimpleClass sc2(100);
+	sc2.ShowData();
+
+	SimpleClass sc3(100,200);
+	sc3.ShowData();
+
+	return 0;
+}
